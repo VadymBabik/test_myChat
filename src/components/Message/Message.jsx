d8e4bfba14ebab72e96b046c9c8241024f95a652
Vadym Babik <vadym.babik@gmail.com>
2021-06-09 11:53:36 +0300

    edit message

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -1,10 +1,28 @@
 import React from "react";
 import s from "./Message.module.scss";
+import { chatUpdate, deleteMessage } from "../../store/chat/actionCreators";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  messageIsEdit,
+  messageSend,
+  messageSetText,
+} from "../../store/message/actionMessage";
 
 const Message = ({ mesage, toggleFavorite }) => {
-  const { avatar, created_at, message, user, isFavorite } = mesage;
+  const dispatch = useDispatch();
+  const { isEdit, messagesText, messagesId } = useSelector(
+    (state) => state.message
+  );
+  const { id, avatar, created_at, message, user, isFavorite } = mesage;
 
+  const edetMassage = (event) => {
+    if (event.key === "Enter") {
+      dispatch(messageSend(messagesText));
+      dispatch(chatUpdate());
+    }
+  };
   const isAutor = JSON.parse(localStorage.getItem("User")) === user;
+  console.log(isAutor);
   return (
     <div className="row container">
       <div
@@ -18,7 +36,21 @@ const Message = ({ mesage, toggleFavorite }) => {
             <h5 className={s.user}>{user}</h5>
             <span className={s.data}>{created_at}</span>
           </div>
-          <p className={s.text}>{message}</p>
+          {isAutor && isEdit && messagesId === id ? (
+            <div className={`${s.inputEdit} input-field`}>
+              <input
+                className={s.inputText}
+                value={messagesText}
+                onChange={(event) =>
+                  dispatch(messageSetText(event.target.value))
+                }
+                onKeyPress={edetMassage}
+                type="text"
+              />
+            </div>
+          ) : (
+            <p className={s.text}>{message}</p>
+          )}
           {!isAutor ? (
             <div className={s.favorite} onClick={toggleFavorite}>
               {isFavorite ? (
@@ -29,8 +61,21 @@ const Message = ({ mesage, toggleFavorite }) => {
             </div>
           ) : (
             <div className={s.editMessage}>
-              <i className={`${s.edit} material-icons`}>edit</i>
-              <i className={`${s.delete} material-icons`}>delete</i>
+              <i
+                onClick={() => {
+                  dispatch(messageSetText(message));
+                  dispatch(messageIsEdit(id));
+                }}
+                className={`${s.edit} material-icons`}
+              >
+                edit
+              </i>
+              <i
+                onClick={() => dispatch(deleteMessage(id))}
+                className={`${s.delete} material-icons`}
+              >
+                delete
+              </i>
             </div>
           )}
         </div>
